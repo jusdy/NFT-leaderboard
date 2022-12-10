e3e7b2056ba38b32d8522564551e6b9c62b30be2
dylandev125 <pauleta0126@gmail.com>
2022-12-10 01:35:59 +0100

    tab polygon border

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -11,22 +11,28 @@ const Tabs: React.FC<TabsProps> = ({
 
     return (
         <ul className="flex mr-[30px] text-center text-xl">
-            <li className={`${tabId === 1 ? 'text-white bg-theme z-30' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} py-2 w-[155px] -mr-[20px] cursor-pointer polyborder`}
+            <li className={`bg-theme relative py-2 w-[155px] h-[45px] -mr-[20px] cursor-pointer polyborder-s`}
                 onClick={() => {onTabClick(1);setTabId(1)}}
             >
-                My NFTs
+                <div className={`${tabId === 1 ? 'text-white bg-theme z-30' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} absolute flex items-center justify-center top-[3px] left-[3px] w-[149px] h-[calc(100%-6px)] polyborder-s2`}>
+                    My NFTs
+                </div>
             </li>
             <li
-                className={`${tabId === 2 ? 'text-white bg-theme z-20' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} py-2 w-[155px] -mr-[20px] cursor-pointer polyborder`}
+                className={`bg-theme relative py-2 w-[155px] -mr-[20px] cursor-pointer polyborder-s`}
                 onClick={() => {onTabClick(2);setTabId(2)}}
             >
-                All NFTs
+                <div className={`${tabId === 2 ? 'text-white bg-theme z-30' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} absolute flex items-center justify-center top-[3px] left-[3px] w-[149px] h-[calc(100%-6px)] polyborder-s2`}>
+                    All NFTs
+                </div>
             </li>
             <li
-                className={`${tabId === 3 ? 'text-white bg-theme z-10' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} py-2 w-[155px] cursor-pointer polyborder`}
+                className={`bg-theme relative py-2 w-[155px] cursor-pointer polyborder-s`}
                 onClick={() => {onTabClick(3);setTabId(3)}}
             >
-                NFT Ledger
+                <div className={`${tabId === 3 ? 'text-white bg-theme z-30' : ' bg-[#292C54] z-0 hover:bg-theme transition-all'} absolute flex items-center justify-center top-[3px] left-[3px] w-[149px] h-[calc(100%-6px)] polyborder-s2`}>
+                    NFT Ledger
+                </div>
             </li>
         </ul>
     )
