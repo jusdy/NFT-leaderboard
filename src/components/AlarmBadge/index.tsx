00d8153a447ba5d20792e0034e58aece1c9ce269
dylandev125 <pauleta0126@gmail.com>
2023-03-15 14:50:53 +0100

    launchpad page mobile respnosive

--- src/components/AlarmBadge/index.tsx
+++ src/components/AlarmBadge/index.tsx
@@ -5,7 +5,7 @@ interface AlarmProps {
 const AlarmBadge = (props: AlarmProps) => {
     return (
         <div className="relative">
-            <img alt="" src="assets/icons/alert.svg"/>
+            <img alt="" className="lg:w-[64px] w-[50px]" src="assets/icons/alert.svg"/>
             <span className="flex justify-center items-center absolute top-0 right-0 w-[20px] h-[20px] rounded-full bg-[#1271FF] text-white text-sm">{props.badgeCount}</span>
         </div>
     )
--- src/layout/Sidebar/MenuItem.tsx
+++ src/layout/Sidebar/MenuItem.tsx
@@ -13,8 +13,8 @@ const MenuItem = ( props: MenuProps ) => {
 
     return (
         <Link to={props.to}>
-            <div className={`poly-bottom-right-s bg-theme border-theme border-t-[3px]`}>
-                <div className={`flex items-center justify-center transition-all w-full h-[70px] poly-bottom-right-s2 bg-[#492D90] font-play text-white text-2xl hover:bg-gradient-to-r from-theme to-[#8840FF] ${props.title?.toLowerCase() === routeName.toLowerCase() ? "bg-gradient-to-r"  : ""}`}>
+            <div className={`poly-bottom-right-s bg-theme border-theme border-t-[3px] lg:bg-opacity-100 bg-opacity-20`}>
+                <div className={`flex items-center justify-center transition-all w-full lg:h-[70px] h-[50px] poly-bottom-right-s2 bg-[#492D90] lg:bg-opacity-100 bg-opacity-20 font-play text-white lg:text-2xl text-lg hover:bg-gradient-to-r from-theme to-[#8840FF] ${props.title?.toLowerCase() === routeName.toLowerCase() ? "bg-gradient-to-r"  : ""}`}>
                     {props.title}
                 </div>
             </div>
